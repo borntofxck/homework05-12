8ca45d05ffaa9e5c42cf8ac868e2d2f9b50fd87f
kvtsuki <117033172+kvtsuki@users.noreply.github.com>
2023-12-03 19:52:50 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = { "Пусть", "задан", "массив", "содержащий", "Строки" };
+            for (int i = 0; i < strings.Length; i++)
+            {
+                string str= strings[i];
+                if ((Char.IsUpper(str, 0)))
+                {
+                    Console.WriteLine($"{i} эллемент начинается с большой буквы {str}");
+                }
+                
+            }
+        }
+    }
+}
